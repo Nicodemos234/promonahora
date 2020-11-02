72277b84c91555b3328a0a9e9cbda68f3b53c510
Nicodemos234 <nicodemos54324@gmail.com>
2020-11-02 12:42:33 -0300

    card width fix

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,10 +2,17 @@
 
 @section('content')
     <div class="row mt-4">
-        <div class="col col-9">
+        <div class="col col-10">
             <div class="row">
                 @include('products.card')
+                @include('products.card')
+                @include('products.card')
+                @include('products.card')
+                @include('products.card')
             </div>
+        </div>
+        <div class="col">
+            Menu Lateral aqui
         </div>
     </div>
 @endsection
