f378868bcf0b83b1ec312e75a2d7f427def7ce89
Nicodemos234 <nicodemos54324@gmail.com>
2020-11-02 15:07:51 -0300

    Store test

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ProductController::class, 'index']);
 Route::get('/oferta/{product}', [ProductController::class, 'show']);
 Route::get('/test', [ProductController::class, 'create']);
+
+Route::get('/test2', [StoreController::class, 'create']);
