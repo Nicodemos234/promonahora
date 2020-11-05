a862dbe92e0204f8b512552d73958007bcbeb1c6
Nicodemos234 <nicodemos54324@gmail.com>
2020-11-05 20:29:42 -0300

    Starting visual of product page

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -6,12 +6,39 @@
 
 @section('content')
 
-    <div class="card d-flex align-items-center mt-4">
-        <h1>{{ $product->name }}</h1>
-
-        <div>
-            <img class="img-fluid" src="{{ $product->pic }}" alt="{{ $product->name }}">
+    <div class="card mt-4 promo-pd-page p-2">
+        <div class="row align-items-center">
+            <div class="col col-3">
+                <a href="{{ $product->link }}" target="_blank">
+                    <img class="img-fluid" src="{{ $product->pic }}" alt="{{ $product->name }}">
+                </a>
+            </div>
+            <div class="col">
+                <h1 class="mb-2">{{ $product->name }}</h1>
+                <div class="promo-pd-price mb-2">
+                    <div class="promo-price-of">
+                        {{ $product->priceof }}
+                    </div>
+                    <div class="promo-price-per">
+                        {{ $product->priceper }}
+                    </div>
+                </div>
+                <a target="_blank" href="{{ $product->link }}" class="btn btn-promo mb-2">
+                    <span class="material-icons-round icon-align">
+                        shopping_basket
+                    </span>
+                    PEGAR PROMO
+                </a>
+            </div>
         </div>
-        {{ $product->store->name }}
+    </div>
+    <div class="card mt-2 p-2">
+        <p>
+            {{ $product->description }} <br>
+            Loja: {{ $product->store->name }} <br>
+            Categorias: @foreach ($product->categories as $category)
+                {{ $category->name }};
+            @endforeach
+        </p>
     </div>
 @endsection
